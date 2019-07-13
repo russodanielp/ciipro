4fac50fe16950c53a161587ed4be2685c75d18cb
Daniel P. Russo <russodanielp@gmail.com>
2019-07-13 10:32:38 -0400

    created new module for making 'DataSet' objects

--- datasets.py
+++ datasets.py
@@ -10,8 +10,8 @@ class DataSet:
         """  to do  """
 
         self.name = name
-        self.cmps = compounds
-        self.activitues = activities
+        self.compounds = compounds
+        self.activities = activities
         self.id_type = identifier_type
 
     def __repr__(self):
@@ -20,24 +20,33 @@ class DataSet:
     def __str__(self):
         return self.__repr__()
 
-    def get_cids(self, identifer: str):
+    def get_cids(self):
         """ converts identifier from the native """
 
         if self.id_type == 'cid':
             return self.cmps
         elif self.id_type == 'cas':
-            return synonyms_db.query_list(self.compounds, 'Synonym', ['CID'])
+            return synonyms_db.query_list(self.cmopounds, 'Synonym', ['CID'])
         elif self.id_type == 'smiles':
             return compounds_db.query_list(self.compounds, 'SMILES Canonical', ['CID'])
         elif self.id_type == 'iupac':
             return compounds_db.query_list(self.compounds, 'IUPAC Name Preferred', ['CID'])
 
+    def get_assays(self):
+        """  """
+
+def make_dataset(dataset_json):
+    """ takes the json of a dataset and makes a dataset object """
+    name = dataset_json['overview']['name']
+    identifier_type = dataset_json['overview']['identifier_type']
+    identifiers = [compound['identifier'] for compound in dataset_json['compounds']]
+    activities = [compound['activity'] for compound in dataset_json['compounds']]
+    return DataSet(name, identifiers, activities, identifier_type)
+
+
+df = pd.read_csv('resources/ER_tutorial/ER_test_can.txt', sep='\t', header=None)
 
 
 
-df = pd.read_csv('resources/ER_tutorial/ER_train_can.txt', sep='\t', header=None)
-
-
-
-er = DataSet('er', df[0], df[1], identifier_type='SMILES')
+er = DataSet('er', df[0].values.tolist(), df[1].values.tolist(), identifier_type='smiles')
 
