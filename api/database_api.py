58f5bc98fa74bcc7c3d110794f6e7a44ba6c4915
Daniel P. Russo <russodanielp@gmail.com>
2019-10-01 15:43:21 -0400

    hoping this fixes the issues

--- api/database_api.py
+++ api/database_api.py
@@ -30,7 +30,7 @@ def get_database(database):
                                         {smiles_string: 1, ACTIVITY_MAPPER[database]: 1, NAME_MAPPER[database]: 1, '_id': 0})
         acute_oral_db = list(acute_oral_db)
         print(acute_oral_db)
-        return jsonify(acute_oral_db)
+        return jsonify(results=acute_oral_db)
 
     else:
         mols_to_send = []
